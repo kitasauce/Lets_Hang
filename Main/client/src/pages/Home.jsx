79f850727535c6cd3edfc51a03eb2d390b209f49
katvela24 <156866399+katvela24@users.noreply.github.com>
2024-07-01 20:19:46 -0400

    added login and signup pages as well as the nav component

--- Main/client/src/pages/Home.jsx
+++ Main/client/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
 import { QUERY_MATCHUPS } from '../utils/queries';
+import Navbar from "../components/Navbar";
 
 const Home = () => {
   const { loading, data } = useQuery(QUERY_MATCHUPS, {
@@ -10,67 +11,70 @@ const Home = () => {
   const matchupList = data?.matchups || [];
 
   return (
-    <div style={{display:"flex"}}>
-    <div className="card bg-white card-rounded w-50">
-      <div className="card-header bg-dark text-center">
-        <h1>Welcome to Let's Hang!</h1>
-      </div>
-      <div className="card-body m-5">
-        <h2>Here is a list of locations you can vote on:</h2>
-        {loading ? (
-          <div>Loading...</div>
-        ) : (
-          <ul className="square">
-            {matchupList.map((matchup) => {
-              return (
-                <li key={matchup._id}>
-                  <Link to={{ pathname: `/matchup/${matchup._id}` }}>
-                    {matchup.tech1} vs. {matchup.tech2}
-                  </Link>
-                </li>
-              );
-            })}
-          </ul>
-        )}
-      </div>
-      <div className="card-footer text-center m-3">
-        <h2>Ready to create a new hang?</h2>
-        <Link to="/matchup">
-          <button className="btn btn-lg btn-danger">Create Meetup!</button>
-        </Link>
+    <div> 
+      <Navbar/>
+      <div style={{ display: "flex" }}>
+        <div className="card bg-white card-rounded w-50">
+          <div className="card-header bg-dark text-center">
+            <h1> Let's Hang</h1>
+          </div>
+          <div className="card-body m-5">
+            <h2>Here is a list of locations you can vote on:</h2>
+            {loading ? (
+              <div>Loading...</div>
+            ) : (
+              <ul className="square">
+                {matchupList.map((matchup) => {
+                  return (
+                    <li key={matchup._id}>
+                      <Link to={{ pathname: `/matchup/${matchup._id}` }}>
+                        {matchup.tech1} vs. {matchup.tech2}
+                      </Link>
+                    </li>
+                  );
+                })}
+              </ul>
+            )}
+          </div>
+          <div className="card-footer text-center m-3">
+            <h2>Ready to create a new hang?</h2>
+            <Link to="/matchup">
+              <button className="btn btn-lg btn-danger">Hang!</button>
+            </Link>
+          </div>
+        </div>
+
+        <div className="card bg-white card-rounded w-50">
+          <div className="card-header bg-dark text-center">
+            <h1>Let's Brawl</h1>
+          </div>
+          <div className="card-body m-5">
+            <h2>Here is a list of locations you can vote on:</h2>
+            {loading ? (
+              <div>Loading...</div>
+            ) : (
+              <ul className="square">
+                {matchupList.map((matchup) => {
+                  return (
+                    <li key={matchup._id}>
+                      <Link to={{ pathname: `/matchup/${matchup._id}` }}>
+                        {matchup.tech1} vs. {matchup.tech2}
+                      </Link>
+                    </li>
+                  );
+                })}
+              </ul>
+            )}
+          </div>
+          <div className="card-footer text-center m-3">
+            <h2>Ready to create a new hang?</h2>
+            <Link to="/matchup">
+              <button className="btn btn-lg btn-danger">Brawl!</button>
+            </Link>
+          </div>
+        </div>
       </div>
     </div>
-
-<div className="card bg-white card-rounded w-50">
-<div className="card-header bg-dark text-center">
-  <h1>Welcome to Let's Hang!</h1>
-</div>
-<div className="card-body m-5">
-  <h2>Here is a list of locations you can vote on:</h2>
-  {loading ? (
-    <div>Loading...</div>
-  ) : (
-    <ul className="square">
-      {matchupList.map((matchup) => {
-        return (
-          <li key={matchup._id}>
-            <Link to={{ pathname: `/matchup/${matchup._id}` }}>
-              {matchup.tech1} vs. {matchup.tech2}
-            </Link>
-          </li>
-        );
-      })}
-    </ul>
-  )}
-</div>
-<div className="card-footer text-center m-3">
-  <h2>Ready to create a new hang?</h2>
-  <Link to="/matchup">
-    <button className="btn btn-lg btn-danger">Create Meetup!</button>
-  </Link>
-</div>
-</div>
-</div>
   );
 };
 
