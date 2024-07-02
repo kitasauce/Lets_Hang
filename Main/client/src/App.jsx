237cfc50caaa398d71c36d5f7de9489d8faafbe2
katvela24 <156866399+katvela24@users.noreply.github.com>
2024-07-02 12:00:45 -0400

    added nav bar component

--- Main/client/src/App.jsx
+++ Main/client/src/App.jsx
@@ -2,6 +2,8 @@ import './App.css';
 import { Outlet } from 'react-router-dom';
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import mapBackground from './assets/images/Lets-hang.png';
+import Navbar from './components/Navbar';
+
 
 const client = new ApolloClient({
   uri: '/graphql',
@@ -11,6 +13,7 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
+      <Navbar></Navbar>
       <div id="map-background" className='bg-fixed bg-cover bg-center' style={{backgroundImage:`url(${mapBackground})`, backgroundSize:"100% auto", backgroundRepeat: "no-repeat", height: "1000px"}}>
     <Outlet />
       </div>
