505747484830ea77dbbaea476e5b84cbcaa9bd00
katvela24 <156866399+katvela24@users.noreply.github.com>
2024-07-02 12:49:24 -0400

    create footer.jsx and footer.css and modified app.jsx and style sheets for login, signup and navbar

--- Main/client/src/App.jsx
+++ Main/client/src/App.jsx
@@ -3,6 +3,7 @@ import { Outlet } from 'react-router-dom';
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import mapBackground from './assets/images/Lets-hang.png';
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 
 const client = new ApolloClient({
@@ -13,11 +14,13 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-      <Navbar></Navbar>
-      <div id="map-background" className='bg-fixed bg-cover bg-center' style={{backgroundImage:`url(${mapBackground})`, backgroundSize:"100% auto", backgroundRepeat: "no-repeat", height: "1000px"}}>
-    <Outlet />
+      <div className='app'>
+        <Navbar></Navbar>
+        <div id="map-background" className='bg-fixed bg-cover bg-center' style={{ backgroundImage: `url(${mapBackground})`, backgroundSize: "100% auto", backgroundRepeat: "no-repeat", height: "1000px" }}>
+          <Outlet />
+        </div>
+        <Footer></Footer>
       </div>
-  
     </ApolloProvider>
   );
 }
